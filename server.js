fd048aa0a39128b3f0abbfa87d60d00641f9f7e2
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-10 14:28:45 -0700

    server now opens after connecting to the database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+//Import the code block responsible for connecting to the database
+const sequelize = require('./routes/config/connection');
 
 // Binds to the nearest available port number if 3001 is unavailable
 const PORT = process.env.PORT || 3001;
@@ -7,6 +9,10 @@ const app = express();
 
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
+// First we connect to the database
+sequelize.sync().then( () => {
+    // Then we open the server
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+    })
 })
